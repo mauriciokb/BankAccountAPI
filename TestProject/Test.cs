e2492eb8514422835dabe8c8bfc30c742ef303d5
mauriciokb <mauriciokbonatto@gmail.com>
2020-09-13 16:28:37 -0300

    Adding the unit test project
    Creating unit test project.
    Moving api project to a new folder.
    Creating a solution and adding both projects.

---
+++ TestProject/Test.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using BankAccountWebAPI;
+
+namespace TestProject
+{
+    public class Test
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }      
+    }
+}
